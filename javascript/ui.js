9fe8281f16c99451a115654cf9975c33a68fd8c1
vkaszuba <codeword.vika@gmail.com>
2020-02-10 20:42:58 -0800

    Fixing id changes in JS

--- javascript/ui.js
+++ javascript/ui.js
@@ -1,26 +1,33 @@
-$('.ui.dropdown')
-  .dropdown();
+let nativeCurrency = 'USD';
 
-  $('#nativeCurrencyModal')
-  .modal('show')
+$('.ui.dropdown').dropdown();
 
+$('#nativeModal').modal('show')
 
 // Clicking on native currency pops initial modal back up
-$('#changeNativeCurrency').click(function(){
-  $('#nativeCurrencyModal').modal('show');    
-});
-
-
-// Changes native currency to Euro
-$('#nativeCurrencyEuro').click(function(){
-  $('#changeNativeCurrency').text("Euro")
-  $('#nativeCurrencyModal')
-  .modal('hide')
+$('#displayNative').click(function(){
+  $('#nativeModal').modal('show');
 });
 
 // Changes native currency to Euro
-$('#convertedCurrencyEuro').click(function(){
-  $('#convertedCurrency').text("Euro")
-  $('#convertedCurrencyModal')
-  .modal('show')
+$(`#nativeBtn-${nativeCurrency}`).click(function(){
+  $('#displayNative').text(nativeCurrency);
+  $('#nativeModal').modal('hide');
+  rateExchange();
 });
+
+// Changes coverted currency to Euro
+$(`#convertedList-EUR`).click(function(){
+  $('#convertedModal').modal('show')
+});
+
+// Getting the Exchange Rate API
+function rateExchange(){
+  url = "https://api.ratesapi.io/api/latest?base=" + nativeCurrency;
+  $.ajax({
+    url,
+    method: "GET"
+  }).then(function(response){
+    console.log(response);
+  })
+}
