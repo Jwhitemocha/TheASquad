7e3c7ec27784f5d40c9a9abd09b7698b1ee2826e
Jwhitemocha <jtvmo4@gmail.com>
2020-02-12 20:17:51 -0800

    testing onclick functions for hello

--- jasonJS.js
+++ jasonJS.js
@@ -1,13 +1,30 @@
 // Getting helloSalut API 
 
-var languageCode = [
-    "en", //english
-    "fr", //french
-    "ja", //japanese
-    "es", //spanish
-    "de", //german
-]
-var helloUrl = "https://fourtonfish.com/hellosalut/?lang=" + languageCode;
+//var languageCode = [
+ //   "en", //english
+  //  "fr", //french
+  //  "ja", //japanese
+  //  "es", //spanish
+  //  "de", //german
+// ]
+
+//Getting Country Class via click function **note Alert is just to test**
+$(".fr").click(function() { 
+    alert('France Was Clicked');
+});
+$(".en").click(function() { 
+    alert('USA was clicked');
+});
+$(".ja").click(function() { 
+    alert('JAPAN was clicked');
+});
+$(".en-gb").click(function() { 
+    alert('England was clicked');
+});
+
+var languageCode = $(".item").click(function();
+
+var helloUrl = "https://fourtonfish.com/hellosalut/?lang=" + languageCode; 
 
 $.ajax({
     url: helloUrl,
@@ -15,5 +32,5 @@ $.ajax({
 }).then(function(response) { 
     console.log(helloUrl);
     console.log(response.hello);
-})
+});
 
