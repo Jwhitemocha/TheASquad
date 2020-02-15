0d135967abe9b8508d6cba7ff9065666f9f4d35f
Jwhitemocha <jtvmo4@gmail.com>
2020-02-15 12:48:25 -0800

    added flags

--- jasonJS.js
+++ jasonJS.js
@@ -7,7 +7,6 @@ $(".item").click(function() {
 
     languageCode = $(this).attr('class').split(" ")[0]
 
-    console.log(languageCode);
     var helloUrl = "https://fourtonfish.com/hellosalut/?lang=" + languageCode; 
 
     $.ajax({
@@ -19,23 +18,27 @@ $(".item").click(function() {
 
     $('#displayConverted').text(response.hello);
 
-    console.log(helloUrl);
-    console.log(response.hello);
 // GET JAPANESE CHARACTERS & DISPLAY PROPER CHARACTERS      
-var japhello = "こんにちは (Kon'nichiwa)"; 
+    var japhello = "こんにちは (Kon'nichiwa)"; 
+
     if (response.hello === "&#12371;&#12435;&#12395;&#12385;&#12399;") { 
         $('#displayConverted').html(japhello); 
-
-};
-
-
+    };
+});
 });
 
-    
-    // こんにちは -- 'Kon'nichiwa 
 
-});
+// Countryflag.io API (for flag) "JP" = JAPAN || "FR" = FRANCE  || "US" = USA  || "GB" = UNITED KINGDOM
 
+// countryCode = "JP";
+$(".item").click(function() { 
+$('#flagPic').empty() 
+    var countryCode = $(this).attr("flag");
+    var flags = "<img src=https://www.countryflags.io/" + countryCode + "/flat/64.png>"
+
+    $('#flagPic').append(flags);
+
+}); 
 
 
 
