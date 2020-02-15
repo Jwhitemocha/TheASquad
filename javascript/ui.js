5c00df69369713822de020a31d4e40cc57565893
Marcus Young <monkeyyoung3@gmail.com>
2020-02-15 13:38:49 -0800

    added last week's rate to secondary modal

--- javascript/ui.js
+++ javascript/ui.js
@@ -1,8 +1,10 @@
 let nativeCurrency;
 let convertedCurrency;
 let convertedRate;
+let lastWeekConvertedRate;
 let nativeSymbol;
 let convertedSymbol;
+const lastWeek = moment().subtract(7, 'days').format('YYYY-MM-DD');
 
 $('.ui.dropdown').dropdown();
 
@@ -18,7 +20,7 @@ $('.nativeBtn').click(function(){
   nativeCurrency = $(this).attr('id').slice(-3);
   nativeSymbol = $(this).attr('symbol');
   $('#displayNative').text(nativeCurrency);
-  $("#nativeRate").text(nativeSymbol + 1);
+  $(".nativeRate").text(nativeSymbol + 1);
   $('#nativeModal').modal('hide');
 })
 
@@ -31,28 +33,40 @@ $(`.item`).click(function(){
 
 // Getting the Exchange Rate API
 function rateExchange(){
-  url = "https://api.ratesapi.io/api/latest?base=" + nativeCurrency;
+  let currentUrl = "https://api.ratesapi.io/api/latest?base=" + nativeCurrency;
+  let lastWeekUrl = "https://api.ratesapi.io/api/" + lastWeek + "?base=" + nativeCurrency;
+
   $.ajax({
-    url,
+    url: currentUrl,
     method: "GET"
   }).then(function(response){
-    $('#convertedModal').modal('show');
-    $('#nativeCurrency').text(nativeCurrency);
-    $('#convertedCurrency').text(convertedCurrency);
-    
-    convertedRate = roundedToTwoDec(response.rates[convertedCurrency])
-    if(nativeCurrency === convertedCurrency){
-      convertedRate = 1
-    }
-    $('#convertedRate').text(convertedSymbol + convertedRate);
+    $.ajax({
+      url: lastWeekUrl,
+      method: "GET"
+    }).then(function(response2){
+        $('#convertedModal').modal('show');
+        $('#nativeCurrency').text(nativeCurrency);
+        $('#convertedCurrency').text(convertedCurrency);
+        
+        convertedRate = roundedToTwoDec(response.rates[convertedCurrency])
+        if(nativeCurrency === convertedCurrency){
+          convertedRate = 1
+        }
+        $('#convertedRate').text(convertedSymbol + convertedRate);
 
-    $('#convertBtn').click(function(){
-      let convertedInput = roundedToTwoDec($('#checkNative').val() * response.rates[convertedCurrency]);
-      $('#checkConverted').val(convertedInput);
-    })
+        $('#convertBtn').click(function(){
+          let convertedInput = roundedToTwoDec($('#checkNative').val() * response.rates[convertedCurrency]);
+          $('#checkConverted').val(convertedInput);
+        })
+        console.log(response2);
+        lastWeekConvertedRate = roundedToTwoDec(response2.rates[convertedCurrency])
+        $('#convertedPreviousRate').text(convertedSymbol + lastWeekConvertedRate);
+      })
   })
 }
 
 function roundedToTwoDec(number){
   return Math.round(number * 100) / 100;
 }
+
+
